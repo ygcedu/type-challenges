65fe9732c8f7c9de0893dcf8456af041bcca81cc
Anderson. J <22438127+andersonjoseph@users.noreply.github.com>
2022-05-03 02:07:38 +0000

    feat(question): add #9616 - Parse URL Params

---
+++ questions/09616-medium-parse-url-params/test-cases.ts
@@ -0,0 +1,12 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+import { ExpectFalse, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<true, true>>,
+  Expect<Equal<ParseUrlParams<''>, never>>,
+  Expect<Equal<ParseUrlParams<':id'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/'>, 'id'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/:user'>, 'id' | 'user'>>,
+  Expect<Equal<ParseUrlParams<'posts/:id/:user/like'>, 'id' | 'user'>>,
+]
